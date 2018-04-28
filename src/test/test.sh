4451a7036db009a0959b95d90c8324412d93fdc9
Jakub Bujak <jakub.jbujak@gmail.com>
2018-04-28 16:46:46 +0200

    Add tests

---
+++ src/test/test.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+for file in $(ls *.jb)
+do
+    ../interpreter $file >out 2>err
+    code=$?
+    if [ -f $file.err ]
+    then expected_code=1
+    else expected_code=0
+    fi
+    if $(diff $file.out out) && [ $code = $expected_code ]
+    then
+        echo -e $file ${GREEN}OK${NC}
+    else
+        echo -e $file ${RED}ERROR${NC}
+    fi
+    rm out
+    rm err
+done
